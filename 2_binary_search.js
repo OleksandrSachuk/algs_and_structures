ecda2be9fbfbf823c6004165a1207ce718d6b6ed
Oleksandr Sachuk <osachuk@playtika.com>
2022-02-16 15:26:13 +0200

    Add binarySearch

---
+++ 2_binary_search.js
@@ -0,0 +1,33 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let count = 0;
+
+function binarySearch(array, item) {
+    let start = 0;
+    let end = array.length;
+    let position = -1;
+    let middle;
+    let found = false;
+
+    while (found === false && start <= end) {
+        count += 1;
+        middle = Math.floor((start + end) / 2);
+
+        if (array[middle] === item) {
+            found = true
+            position = middle;
+
+            return position
+        }
+
+        if (item < array[middle]) {
+            end = middle - 1
+        } else {
+            start = middle + 1
+        }
+    }
+
+    return position;
+}
+
+console.log('binarySearch index', binarySearch(array, 1));
+console.log('count = ', count);
